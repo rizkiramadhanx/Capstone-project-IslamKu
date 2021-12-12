25f280917590024cf2c9463ef17077745dd5552e
Reza <rezardiansyah2706@gmail.com>
2021-12-12 15:23:13 +0800

    responsive dan burger button

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,2 +1,12 @@
 import 'regenerator-runtime';
 import '../styles/style.css';
+import '../styles/responsive.css';
+import App from './views/App';
+
+const app = new App({
+  button: document.querySelector('#drawer'),
+  drawer: document.querySelector('#menu'),
+  content: document.querySelector('#content'),
+});
+
+export default app;
---
+++ src/scripts/views/app.js
@@ -0,0 +1,20 @@
+import DrawerInitiator from "../utils/Drawer-initiator";
+
+class App {
+  constructor({ button, drawer, content }) {
+    this._button = button;
+    this._drawer = drawer;
+    this._content = content;
+
+    this._initialAppShell();
+  }
+
+  _initialAppShell() {
+    DrawerInitiator.init({
+      button: this._button,
+      drawer: this._drawer,
+      content: this._content,
+    });
+  }
+}
+export default App;
